fcf03697462483c976485c2d201e1afaf2334b9b
Dennis <dennis1989999@gmail.com>
2021-05-25 20:15:54 +0800

    HW6

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import DatePickers from './DatePickers.js';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DatePickers />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
